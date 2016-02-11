acfa5def6f2fba300c65d0e0bd04e7d0979f10b9
Svetlin Nakov <github@nakov.com>
2016-02-11 15:12:25 +0200

    Added PureTextCheckerTest.cs

---
+++ PureTextCheckerTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class PureTextCheckerTest
+{
+    static void Main()
+    {
+        var checker = new PureTextChecker();
+        string input = "";
+        string expectedOutput =
+            " Търсена  сума:500 " + "\r\n" +
+            "Довиждане" + "\r\n";
+        string userOutput =
+            " Търсена  сума = 500.Довиждане!";
+        var result = checker.Check(input, userOutput, expectedOutput, false);
+        Console.WriteLine(result.IsCorrect);
+        Console.WriteLine(result.CheckerDetails.Comment);
+        Console.WriteLine(result.CheckerDetails.UserOutputFragment);
+        Console.WriteLine(result.CheckerDetails.ExpectedOutputFragment);
+    }
+}
